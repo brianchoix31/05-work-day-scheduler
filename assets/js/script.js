2814306c2877c25e4f7143df51f784aebd80ec04
Brian Choi <brianchoix31@gmail.com>
2022-08-09 00:01:57 -0400

    troubleshooting to retrieve items to append from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,49 @@
-var day = moment();
-$('#day').text(day.format('[Today is] dddd, MMM do, YYYY.'));
+var today = moment();
+$('#day').text(today.format('[Today is] dddd, MMM Do, YYYY.'));
 var time = moment();
-$('#time').text(time.format('h:mm A'));
+$('#time').text(time.format('[Time is] h:mm:ss A.'));
 
-var rowHour = $('.row').val('id');
-console.log(rowHour);
-var currentHour = day.format('h');
-console.log(currentHour);
+var saveBtn = $('.saveBtn');
+saveBtn.on('click', function() { 
+    var time = $(this).parent().parent().attr('id');
+    console.log(time);
+    var task = $(this).parent().siblings('.note').children('.task').val();
+    console.log(task);
 
-if (currentHour > rowHour) {
-    $('.row').addClass('past');
-    $('.hour').css('border-right', '3px solid var(--red');
-} else if (currentHour === rowHour) {
-    $('.row').addClass('present');
-    $('.hour').css('border-right', '3px solid var(--yellow');
-} else {
-    $('.row').addClass('future')
-    $('.hour').css('border-right', '3px solid white');
+    localStorage.setItem(time, task);
+})
+
+function noteBgColor() {
+    var currentHour = moment().hours();
+    console.log(currentHour);
+
+    $('.note').each(function() {
+        var rowHour = parseInt($(this).parent().attr('id'));
+        console.log(rowHour);
+        // console.log($(this));
+     
+     if (currentHour === rowHour) {
+        $(this).addClass('present');
+        $(this).css('border-left', '5px solid var(--green');
+    } else if (currentHour > rowHour) {
+        $(this).addClass('past');
+        $(this).css('border-left', '5px solid var(--red');
+    } else {
+        $(this).addClass('future');
+        $(this).css('border-left', '5px solid var(--beige');
+    }
+
+    })
 }
+noteBgColor();
+// setInterval(noteBgColor, 1000);
 
-
-
-
-
-
-// var submitBtnEl = $('');
-// submitBtnEl.on('click', function () {
-//     submitDisplayEl.text();
-// });
+$('#9 .task').val(localStorage.getItem('9'));
+$('#10 .task').val(localStorage.getItem('10'));
+$('#11 .task').val(localStorage.getItem('11'));
+$('#12 .task').val(localStorage.getItem('12'));
+$('#13 .task').val(localStorage.getItem('13'));
+$('#14 .task').val(localStorage.getItem('14'));
+$('#15 .task').val(localStorage.getItem('15'));
+$('#16 .task').val(localStorage.getItem('16'));
+$('#17 .task').val(localStorage.getItem('17'));
